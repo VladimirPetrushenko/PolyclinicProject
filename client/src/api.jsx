cf02e2bfb0748376d989f6bd67eeacac1980e324
VladimirPetrushenko <VladimirVPetrushenko>
2021-04-17 20:44:56 +0300

    docker compose

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,30 +1,33 @@
+var cur = window.location;
+const {hostname} = cur;
+
 export const getAllDoctors= async () => {
-    const response = await fetch("http://policlinic:8080/api/Doctors/");
+    const response = await fetch(`http://${hostname}:8080/api/Doctors/`);
     return await response.json();
 }
 
 export const getDoctorById= async (id) => {
-    const response = await fetch(`http://policlinic:8080/api/Doctors/${id}`);
+    const response = await fetch(`http://${hostname}:8080/api/Doctors/${id}`);
     return await response.json();
 }
 
 export const getAllPatients= async () => {
-    const response = await fetch("http://policlinic:8080/api/Patients/");
+    const response = await fetch(`http://${hostname}:8080/api/Patients/`);
     return await response.json();
 }
 
 export const getPatientById= async (id) => {
-    const response = await fetch(`http://policlinic:8080/api/Patients/${id}`);
+    const response = await fetch(`http://${hostname}:8080/api/Patients/${id}`);
     return await response.json();
 }
 
 
 export const getAllMedicalCards= async () => {
-    const response = await fetch("http://policlinic:8080/api/MedicalCards");
+    const response = await fetch(`http://${hostname}:8080/api/MedicalCards`);
     return await response.json();
 }
 
 export const getMedicalCardById= async (id) => {
-    const response = await fetch(`http://policlinic:8080/api/MedicalCards/${id}`);
+    const response = await fetch(`http://${hostname}:8080/api/MedicalCards/${id}`);
     return await response.json();
 }
