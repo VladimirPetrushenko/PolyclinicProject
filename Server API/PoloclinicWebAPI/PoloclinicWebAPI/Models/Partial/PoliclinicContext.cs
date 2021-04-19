50b8f980c6fdae8b128d4598f1d0f67c960a5cd3
Vladimir Petrushenko <barrierofdust@gmail.com>
2021-04-19 10:01:01 +0300

    add  PoliclinicContext

---
+++ Server API/PoloclinicWebAPI/PoloclinicWebAPI/Models/Partial/PoliclinicContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoloclinicWebAPI.Models.DBContext
+{
+    //без этого метода не заводится, срабатывает lazy loading и не подгружаются внешние ключи таблиц
+    public partial class PoliclinicContext
+    {
+        private void InitDictionaries()
+        {
+            Qualifications.ToList();
+            Specializations.ToList();
+            Diagnoses.ToList();
+            Patients.ToList();
+            Doctors.ToList();
+            MedicalCards.ToList();
+        }
+    }
+}
